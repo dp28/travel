3c0bd02b544780ff6a022be8c540b36fea048258
Daniel Patterson <dan24patt@gmail.com>
2017-12-24 00:42:49 +0000

    Enable compiling assets into a release directory
    This will allow the state of the front end to be committed at each
    release point. There is no need for fingerprinting because each released
    version will not change.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,15 @@
+const path = require('path')
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+config.devtool = 'cheap-module-source-map'
+
+const versionTag = process.env.RELEASE_VERSION
+if (versionTag) {;
+  config.output.filename = `[name].js`
+  config.plugins[1].filename = `[name].css`
+  config.output.path = path.join(__dirname, '../../public/releases', versionTag)
+  config.output.publicPath = `/releases/${versionTag}`
+}
+
+module.exports = config
