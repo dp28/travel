2cc5a6bf9d883e6c8607858bb36d6db6ad43e8c1
Daniel Patterson <dan24patt@gmail.com>
2017-12-26 23:00:06 +0000

    Enable GraphiQL to test queries through UI

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,5 +1,7 @@
 class GraphqlController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   def resolve
     query_variables = params[:variables] || {}
     result = Schema.execute(
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root controller: 'apps', action: 'current'
   post '/graphql', controller: 'graphql', action: 'resolve'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
 end
