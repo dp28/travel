d8cb174c86f169f4213b719319a3581c30f2823a
Daniel Patterson <dan24patt@gmail.com>
2018-01-11 11:53:27 +0000

    Add basic currency representation
    These have hard-coded exchange rates! Oh no!
    
    It's fine, these are the rates at the time of our trip. This is how much
    things cost.

---
+++ app/models/currency.rb
@@ -0,0 +1,48 @@
+# Does not extend ApplicationModel as not stored in database
+class Currency
+
+  attr_reader :code, :name, :symbol, :exchange_rate_to_dollars
+
+  def initialize(code:, name:, symbol:, exchange_rate_to_dollars:)
+    @code = code.to_sym
+    @name = name
+    @symbol = symbol
+    @exchange_rate_to_dollars = exchange_rate_to_dollars
+  end
+
+  USD = Currency.new(
+    code: :USD,
+    name: 'dollars',
+    symbol: '$',
+    exchange_rate_to_dollars: 1.0
+  )
+
+  GBP = Currency.new(
+    code: :GBP,
+    name: 'pounds',
+    symbol: '£',
+    exchange_rate_to_dollars: 1.34670
+  )
+
+  THB = Currency.new(
+    code: :THB,
+    name: 'baht',
+    symbol: '฿',
+    exchange_rate_to_dollars: 0.0312192
+  )
+
+  VND = Currency.new(
+    code: :VND,
+    name: 'dong',
+    symbol: '₫',
+    exchange_rate_to_dollars: 0.0000440622
+  )
+
+  ALL = [USD, GBP, THB, VND].freeze
+
+  def self.find_by_code(code)
+    code_sym = code.to_sym
+    ALL.detect { |currency| currency.code == code_sym }
+  end
+
+end
---
+++ spec/models/currency_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe Currency do
+  shared_examples_for 'a currency' do
+    describe '#code' do
+      subject { currency.code }
+      it { should eq code }
+    end
+
+    describe '#name' do
+      subject { currency.name }
+      it { should eq name }
+    end
+
+    describe '#exchange_rate_to_dollars' do
+      subject { currency.exchange_rate_to_dollars }
+      it { should eq exchange_rate_to_dollars }
+    end
+
+    describe '#symbol' do
+      subject { currency.symbol }
+      it { should eq symbol }
+    end
+
+    it 'should be discoverable by code' do
+      expect(Currency.find_by_code(code)).to be currency
+    end
+
+    it 'should be discoverable by string code' do
+      expect(Currency.find_by_code(code.to_s)).to be currency
+    end
+  end
+
+  describe 'Currency::USD' do
+    subject(:currency) { Currency::USD }
+
+    it_should_behave_like 'a currency' do
+      let(:code) { :USD }
+      let(:name) { 'dollars' }
+      let(:symbol) { '$' }
+      let(:exchange_rate_to_dollars) { 1.0 }
+    end
+  end
+
+  describe 'Currency::GBP' do
+    subject(:currency) { Currency::GBP }
+
+    it_should_behave_like 'a currency' do
+      let(:code) { :GBP }
+      let(:name) { 'pounds' }
+      let(:symbol) { '£' }
+      let(:exchange_rate_to_dollars) { 1.34670 }
+    end
+  end
+
+  describe 'Currency::THB' do
+    subject(:currency) { Currency::THB }
+
+    it_should_behave_like 'a currency' do
+      let(:code) { :THB }
+      let(:name) { 'baht' }
+      let(:symbol) { '฿' }
+      let(:exchange_rate_to_dollars) { 0.0312192 }
+    end
+  end
+
+  describe 'Currency::VND' do
+    subject(:currency) { Currency::VND }
+
+    it_should_behave_like 'a currency' do
+      let(:code) { :VND }
+      let(:name) { 'dong' }
+      let(:symbol) { '₫' }
+      let(:exchange_rate_to_dollars) { 0.0000440622 }
+    end
+  end
+end
