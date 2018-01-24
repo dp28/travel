1a4b105c800e0ef2f3ea5254672e1b7929b216e9
Daniel Patterson <dan24patt@gmail.com>
2018-01-24 07:55:52 +0000

    Show basic posts for each day on the main page
    This is the main purpose of the site, but a very basic version of it.

--- app/javascript/travel/components/App/App.jsx
+++ app/javascript/travel/components/App/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ConnectedReleases } from '../Releases/Releases'
+import { ConnectedDays } from '../Days/Days'
 
 export const App = () => (
   <div>
     <div>Check out the <a href="/graphiql">API</a></div>
-    <ConnectedReleases />
+    <ConnectedDays />
   </div>
 )
---
+++ app/javascript/travel/components/Days/Days.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+export const Days = ({ days, loading }) => {
+  if (loading) {
+    return <div className="loading">Loading ...</div>
+  }
+  else {
+    return (
+      <ul className="Days">
+          {days.map(day => (
+            <li key={day.number}>
+              <h2 className="Day--title">Day {day.number}</h2>
+              {day.post.content}
+            </li>
+          ))}
+      </ul>
+    )
+  }
+}
+
+export const ConnectedDays = graphql(gql`
+  query {
+    days {
+      edges {
+        node {
+          number
+          post {
+            content
+          }
+        }
+      }
+    }
+  }
+`, {
+  props: ({ data: { days, loading } }) => ({
+    loading,
+    days: loading ? [] : days.edges.map(edge => edge.node)
+  })
+})(Days)
+
