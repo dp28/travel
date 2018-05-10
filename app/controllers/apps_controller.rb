76327f66cb5c13219ac421acb2ced29ed8ac5ec5
Daniel Patterson <dan24patt@gmail.com>
2018-05-11 00:28:20 +0100

    Enable swapping to previous versions of the site
    This allows users to see how the site has developed over time and view
    each step.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -7,10 +7,36 @@ class AppsController < ApplicationController
     @release || raise_not_found
   end
 
+  def history
+    @json_releases = present_releases ReleaseRepository.search
+  end
+
 private
 
   def raise_not_found
     raise ActionController::RoutingError, 'Not Found'
+  end
+
+  def present_releases(releases)
+    releases.reverse.map.with_index do |release, index|
+      {
+        version: release.version,
+        time: release.time.to_s[0..9],
+        number: index + 1,
+        description: render_markdown(release.description)
+      }
+    end.to_json
+  end
+
+  def render_markdown(markdown)
+    markdown_engine.render markdown
+  end
+
+  def markdown_engine
+    @markdown_engine ||= begin
+      renderer = Redcarpet::Render::HTML.new
+      Redcarpet::Markdown.new renderer
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root controller: 'apps', action: 'current'
+  get '/history', controller: 'apps', action: 'history'
   get '/versions/:version', controller: 'apps', action: 'version'
   post '/graphql', controller: 'graphql', action: 'resolve'
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
