12de3cb1845dcbab77b29ee382b11da9164c80d6
Daniel Patterson <dan24patt@gmail.com>
2022-10-06 14:29:31 -0400

    Store all GraphQL requests
    So that the client can be completely independent from the server

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,10 +3,32 @@ class GraphqlController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def resolve
-    query_variables = params[:variables] || {}
     uri = URI('http://www.rndtravels.com/graphql')
-    res = Net::HTTP.post(uri, params.to_json, "Content-Type" => "application/json")
-    render json: JSON.parse(res.body)
+    res = Net::HTTP.post(uri, safe_params.to_json, "Content-Type" => "application/json")
+    body = JSON.parse(res.body)
+    store(body)
+    render json: body
+  end
+
+  private
+
+  def safe_params
+    @safe_params ||= params.permit!
+  end
+
+  def store(body)
+    key = { variables: safe_params[:variables] || {}, query: safe_params[:query] }.to_json
+    cache[key] = body
+    File.write(cache_file, cache.to_json)
+    puts "CACHE KEYS: #{cache.keys.size}"
+  end
+
+  def cache
+    @@cache ||= JSON.parse(File.read(cache_file)) || {}
+  end
+
+  def cache_file
+    'graphql_cache.json'
   end
 
 end
