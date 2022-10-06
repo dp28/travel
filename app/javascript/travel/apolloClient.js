b6c782587bb2639993ce4dd6aee17751e8aec0f1
Daniel Patterson <dan24patt@gmail.com>
2022-10-06 15:48:14 -0400

    Stop graphql requests to the server
    All data for the site is now loaded ahead of time. This will increase
    download time, but makes it possible for this to be an entirely static
    site with no web server. This makes it much easier to move away from
    Heroku.

--- app/javascript/travel/apolloClient.js
+++ app/javascript/travel/apolloClient.js
@@ -1,8 +1,38 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import gql from "graphql-tag";
+
+const DataSource = new InMemoryCache();
+
+prepopulate(DataSource);
 
 export const Client = new ApolloClient({
   link: new HttpLink(),
-  cache: new InMemoryCache()
-})
+  cache: DataSource,
+  defaultOptions: {
+    query: {
+      fetchPolicy: "cache-only",
+    },
+    watchQuery: {
+      fetchPolicy: "cache-only",
+    },
+  },
+});
+
+function prepopulate(cache) {
+  if (!RnDTravelsData) {
+    console.error("Data not loaded");
+    return;
+  }
+
+  Object.entries(RnDTravelsData).forEach(([query, variablesToResults]) => {
+    Object.entries(variablesToResults).forEach(([variablesString, result]) => {
+      cache.writeQuery({
+        query: gql(query),
+        variables: JSON.parse(variablesString),
+        data: result.data,
+      });
+    });
+  });
+}
