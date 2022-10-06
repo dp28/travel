49be29f989a5770f00fb722f4e2d7ca78c030f43
Daniel Patterson <dan24patt@gmail.com>
2022-10-06 12:28:40 -0400

    Disable database interactions
    To move towards removing the server entirely

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,9 @@ require 'rails'
 # Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_job/railtie'
-require 'active_record/railtie'
+
+# Deliberately removed to prevent database access
+# require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
